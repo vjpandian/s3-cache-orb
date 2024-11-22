643e775e2b879ad96bc2e771a785a8398161dab1
Vijay Pandian <ananth.vijay@gmail.com>
2024-11-21 23:39:31 -0500

    simplified restore

--- src/scripts/restore-cache.sh
+++ src/scripts/restore-cache.sh
@@ -5,20 +5,15 @@ echo "Cache Path is $CACHE_PATH"
 
 # Check if CACHE_KEY and CACHE_PATH are set
 if [ -z "$CACHE_KEY" ] || [ -z "$CACHE_PATH" ]; then
-  echo "CACHE_KEY or CACHE_PATH is not set. Exiting..."
+  echo "Error: CACHE_KEY or CACHE_PATH is not set. Exiting..."
   exit 1
 fi
 
 # Check if the cache path already exists locally
 if [ -d "$CACHE_PATH/$CACHE_KEY" ]; then
-    echo "Cache already exists locally at $CACHE_PATH. Skipping download."
+    echo "Cache already exists locally at $CACHE_PATH/$CACHE_KEY. Skipping download."
     exit 0
 fi
-
-# Configure S3 settings
-aws configure set default.s3.max_concurrent_requests "$MAX_CONCURRENT_REQUESTS"
-aws configure set default.s3.max_queue_size "$MAX_QUEUE_SIZE"
-aws configure set default.s3.multipart_threshold "$S3_MULTIPART_THRESHOLD"
 
 # Check if the archive exists in the S3 bucket
 echo "Checking if s3://$BUCKET_NAME/$CACHE_KEY/$CACHE_KEY.tar.gz exists..."
@@ -26,20 +21,13 @@ if aws s3 ls "s3://$BUCKET_NAME/$CACHE_KEY/$CACHE_KEY.tar.gz" > /dev/null 2>&1; 
     echo "Cache archive found. Downloading..."
     
     # Download the archive from the S3 bucket
-    if aws s3 cp "s3://$BUCKET_NAME/$CACHE_KEY/$CACHE_KEY.tar.gz" "$CACHE_KEY.tar.gz"; then
-        echo "Cache archive downloaded: $CACHE_KEY.tar.gz"
-
-        if tar -xzf "$CACHE_KEY.tar.gz" .; then
-            echo "Cache restored...."
-        else
-            echo "Failed to extract cache archive."
-            exit 1
-        fi
+    if aws s3 cp "s3://$BUCKET_NAME/$CACHE_KEY/$CACHE_KEY.tar.gz" "$CACHE_PATH/$CACHE_KEY.tar.gz"; then
+        echo "Cache archive successfully downloaded to $CACHE_PATH/$CACHE_KEY.tar.gz"
     else
-        echo "Failed to download cache archive."
+        echo "Error: Failed to download cache archive from S3."
         exit 1
     fi
 else
-    echo "Cache archive not found in S3. Exiting..."
+    echo "Error: Cache archive not found in S3."
     exit 1
 fi
