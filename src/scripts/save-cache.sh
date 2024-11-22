3ded2efabb3f120dd38939cb50ba21535acaa9ef
Vijay Pandian <ananth.vijay@gmail.com>
2024-11-21 22:45:33 -0500

    replace vars

--- src/scripts/save-cache.sh
+++ src/scripts/save-cache.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo "Cache Path is <<parameters.cache-path>>"
+echo "Cache Path is $CACHE_PATH"
 
 # Check if the cache path exists
-if [ -d "<<parameters.cache-path>>" ]; then
+if [ -d "$CACHE_PATH" ]; then
   
   echo "Cache path exists. Archiving..."
   # Create the archive
-  tar -czf "<<parameters.cache-key>>.tar.gz" "<<parameters.cache-path>>"
-  echo "Archive created: <<parameters.cache-path>>.tar.gz"
+  tar -czf "$CACHE_KEY.tar.gz" "$CACHE_PATH"
+  echo "Archive created: $CACHE_PATH.tar.gz"
 
   # Configure S3 settings
-  aws configure set default.s3.max_concurrent_requests "<<parameters.s3-max-concurrent-requests>>"
-  aws configure set default.s3.max_queue_size "<<parameters.s3-max-queue-size>>"
-  aws configure set default.s3.multipart_threshold "<<parameters.s3-multipart-threshold>>"
+  aws configure set default.s3.max_concurrent_requests "$MAX_CONCURRENT_REQUESTS"
+  aws configure set default.s3.max_queue_size "$MAX_QUEUE_SIZE"
+  aws configure set default.s3.multipart_threshold "$S3_MULTIPART_THRESHOLD"
 
   # Upload the archive to the S3 bucket
-  aws s3 cp "<<parameters.cache-key>>.tar.gz" "s3://<<parameters.bucket-name>>/<<parameters.cache-key>>/<<parameters.cache-key>>.tar.gz"
+  aws s3 cp "$CACHE_KEY.tar.gz" "s3://<<parameters.bucket-name>>/$CACHE_KEY/$CACHE_KEY.tar.gz"
   echo "Cache archive uploaded to S3."
 else
   echo "Cache path does not exist. Skipping upload."
