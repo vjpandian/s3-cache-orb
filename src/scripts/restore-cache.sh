945bf355d2d45eaacc2452d3a3eba740b0ead30f
Vijay Pandian <ananth.vijay@gmail.com>
2024-11-21 23:09:47 -0500

    shellcheck error fix

--- src/scripts/restore-cache.sh
+++ src/scripts/restore-cache.sh
@@ -26,17 +26,13 @@ if aws s3 ls "s3://$BUCKET_NAME/$CACHE_KEY/$CACHE_KEY.tar.gz" > /dev/null 2>&1; 
     echo "Cache archive found. Downloading..."
     
     # Download the archive from the S3 bucket
-    aws s3 cp "s3://$BUCKET_NAME/$CACHE_KEY/$CACHE_KEY.tar.gz" "$CACHE_KEY.tar.gz"
-    
-    if [ -f "$CACHE_KEY.tar.gz" ]; then
+    if aws s3 cp "s3://$BUCKET_NAME/$CACHE_KEY/$CACHE_KEY.tar.gz" "$CACHE_KEY.tar.gz"; then
         echo "Cache archive downloaded: $CACHE_KEY.tar.gz"
 
         # Extract the archive to the specified cache path
         echo "Extracting archive to $CACHE_PATH..."
         mkdir -p "$CACHE_PATH"
-        tar -xzf "$CACHE_KEY.tar.gz" -C "$CACHE_PATH"
-
-        if [ $? -eq 0 ]; then
+        if tar -xzf "$CACHE_KEY.tar.gz" -C "$CACHE_PATH"; then
             echo "Cache restored to $CACHE_PATH."
         else
             echo "Failed to extract cache archive."
