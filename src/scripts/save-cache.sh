e67ce0f2ec51ea6afd3c80e20a1e2a4d11cc93a5
Vijay Pandian <ananth.vijay@gmail.com>
2024-11-21 22:24:28 -0500

    generate new dev version with script updates

--- src/scripts/save-cache.sh
+++ src/scripts/save-cache.sh
@@ -3,9 +3,8 @@
 # Check if the cache path exists
 if [ -d "<<parameters.cache-path>>" ]; then
   echo "Cache path exists. Archiving..."
-
   # Create the archive
-  tar -czf "<<parameters.cache-path>>.tar.gz" -C "<<parameters.cache-path>>" .
+  tar -czf "<<parameters.cache-key>>.tar.gz" "<<parameters.cache-path>>"
   echo "Archive created: <<parameters.cache-path>>.tar.gz"
 
   # Configure S3 settings
@@ -14,7 +13,7 @@ if [ -d "<<parameters.cache-path>>" ]; then
   aws configure set default.s3.multipart_threshold "<<parameters.s3-multipart-threshold>>"
 
   # Upload the archive to the S3 bucket
-  aws s3 cp "<<parameters.cache-path>>.tar.gz" "s3://<<parameters.bucket-name>>/<<parameters.cache-key>>/<<parameters.cache-path>>.tar.gz"
+  aws s3 cp "<<parameters.cache-key>>.tar.gz" "s3://<<parameters.bucket-name>>/<<parameters.cache-key>>/<<parameters.cache-key>>.tar.gz"
   echo "Cache archive uploaded to S3."
 else
   echo "Cache path does not exist. Skipping upload."
